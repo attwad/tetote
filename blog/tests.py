075a0fac189bc3f62e4df9aee31234e802d8ca37
tmu <attwad@users.noreply.github.com>
2026-04-01 22:13:45 +0200

    feat: show empty message when no blog posts exist. Closes https://github.com/attwad/tetote/issues/31

--- blog/tests.py
+++ blog/tests.py
@@ -15,6 +15,13 @@ class BlogViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Test Post")
 
+    def test_post_list_view_empty(self):
+        Post.objects.all().delete()
+        url = reverse("blog:post_list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "We are sorry, there are no blog posts yet.")
+
     def test_post_detail_view(self):
         url = reverse("blog:post_detail", kwargs={"slug": self.post.slug})
         response = self.client.get(url)
